1125487d5ae9d7a9fd68870d796d0469725d4aa3
Rana Muneeb Asad <muneebasad.24@gmail.com>
2021-08-30 19:20:18 +0500

    Add files via upload
    """             ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,   a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8   8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88           "8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88            `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88                88             88                                             ""             88                                                            88                                   ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,   a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8   8b         88 88       d8 88       88 8PP""""""" 88           "8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88            `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88                         88                                                            88            """  Caesar_Cipher An interactive python project for beginners

---
+++ Caesar_Cipher.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 10 13:28:40 2021
+
+@author: Rana Muneeb Asad
+"""
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+print(logo)
+
+def caesar(start_text, shift_amount, cipher_direction):
+    end_text =""
+    if cipher_direction == 'decode':
+            shift_amount *= -1
+    for char in start_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            end_text += alphabet[new_position]
+        else:
+            end_text += char
+    print(f"the {cipher_direction}d text is {end_text}")
+            
+should_countinue = True
+
+while should_countinue:
+    
+    direction = input("type 'encode' to encrypte or type 'decode' to decrypt \n")
+    text = input("type your response: ").lower()
+    shift = int(input("Whats the shift number?: "))
+    shift = shift % 26
+    caesar(start_text = text,shift_amount= shift, cipher_direction= direction)
+    
+    result = input("type 'yes' to continue, 'no' to quite: ")
+    if result == 'no':
+        should_countinue = False
+        print("Good bye")
+        
